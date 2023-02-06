e009c7ab897c5d5da2816c495eb7f39ebe49bea1
sonms <smsms5676@naver.com>
2023-02-06 18:32:04 +0900

    feature/Alarm(..ing)

---
+++ app/src/main/java/com/example/mysolutionchallenge/Helper/AlertReceiver.kt
@@ -0,0 +1,23 @@
+package com.example.mysolutionchallenge.Helper
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+
+class AlertReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        var notificationHelper : NotificationHelper = NotificationHelper(context)
+
+        var bundle = intent?.getBundleExtra("bundle")
+
+        var time = bundle?.getString("time")
+        var content = bundle?.getString("content")
+
+        var nb : NotificationCompat.Builder = notificationHelper.getChannelNotification(time, content)
+
+        //알림 호출 코드
+        notificationHelper.getManager().notify(1, nb.build())
+    }
+}

1f242f46f5781fbb875ab9c19517288ceeba011e
sonms <smsms5676@naver.com>
2023-03-05 20:10:21 +0900

    feature/TextSize Change Theme Set

--- app/src/main/java/com/example/mysolutionchallenge/SearchItemViewActivity.kt
+++ app/src/main/java/com/example/mysolutionchallenge/SearchItemViewActivity.kt
@@ -7,14 +7,45 @@ import android.view.MenuItem
 import com.example.mysolutionchallenge.Model.MedicalData
 import com.example.mysolutionchallenge.Model.PillData
 import com.example.mysolutionchallenge.databinding.ActivitySearchitemviewBinding
+import com.example.mytodolist.SharedPref
 
 class SearchItemViewActivity : AppCompatActivity() {
 
     private lateinit var mBinding : ActivitySearchitemviewBinding
     private var setMedicalContent : MedicalData? = null
+
+    //상태유지
+    var sharedPref : SharedPref? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        sharedPref = SharedPref(this)
+        ///테마 변경///
+        if (sharedPref!!.loadNightModeState()) {
+            setTheme(R.style.darktheme)
+        } else {
+            setTheme(R.style.AppTheme)
+        }
+        if (sharedPref!!.loadSmallModeState()) {
+            setTheme(R.style.smalltextsizetheme)
+        } else {
+            setTheme(R.style.AppTheme)
+        }
+        if (sharedPref!!.loadMiddleModeState()) {
+            setTheme(R.style.mediumtextsizetheme)
+        } else {
+            setTheme(R.style.AppTheme)
+        }
+        if (sharedPref!!.loadLargeModeState()) {
+            setTheme(R.style.largetextsizetheme)
+        } else {
+            setTheme(R.style.AppTheme)
+        }
+        /////////////////////////////////
         mBinding = ActivitySearchitemviewBinding.inflate(layoutInflater)
+
+
         setContentView(mBinding.root)
 
         //supportActionBar!!.setDisplayHomeAsUpEnabled(true)
